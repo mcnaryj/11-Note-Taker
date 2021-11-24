84099b55f095247ca9ed10112313f239b07ff852
Jack McNary <jack.mcnary@gmail.com>
2021-11-24 10:12:26 -0700

    connected to heroku (finally)

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-// GET Route/catchall for homepage
+// GET Route for homepage
 app.get('*', (req, res) => {
     console.log("get *");
     res.sendFile(path.join(__dirname, '/public/index.html'));
@@ -59,3 +59,21 @@ const readAndAppend = (content, file) => {
 app.get('/api/notes', (req, res) =>
     readFromFile('.db/db.json').then((data) => res.json(JSON.parse(data)))
 );
+app.post('/api/notes', (req, res) =>
+    readFromFile('.db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+// heroku hosts our application on the cloud
+
+// GIVEN a note-taking application
+// WHEN I open the Note Taker
+// THEN I am presented with a landing page with a link to a notes page
+// WHEN I click on the link to the notes page
+// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+// WHEN I enter a new note title and the note’s text
+// THEN a Save icon appears in the navigation at the top of the page
+// WHEN I click on the Save icon
+// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+// WHEN I click on an existing note in the list in the left-hand column
+// THEN that note appears in the right-hand column
+// WHEN I click on the Write icon in the navigation at the top of the page
+// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
