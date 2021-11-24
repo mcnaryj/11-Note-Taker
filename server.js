c8279381215bfbfdf659476464c32554c8e3cb99
Jack McNary <jack.mcnary@gmail.com>
2021-11-24 12:45:49 -0700

    pseudo coding delete route

--- server.js
+++ server.js
@@ -75,24 +75,15 @@ app.post('/api/notes', (req, res) => {
 
 
 // DELETE /api/notes/:id
-
-// heroku hosts our application on the cloud
-
-// GIVEN a note-taking application
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+app.delete('/api/notes/:id'), (req, res) => {
+    // we want the userid
+    // then we want to read from the db jason file, promise the return of the data, then parse that data in the fxn
+    // then with our notes variable, we want to filter it, and check to see that the note does not match the id
+    // we want a promise that says ok: true
+    // a catch error function to return a 500 error if we enter the wrong id
 
 
+}
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
