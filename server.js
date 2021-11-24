36eae40debc6a6d17d46b2dcd6e22a4da6fffdde
Jack McNary <jack.mcnary@gmail.com>
2021-11-24 08:50:50 -0700

    firing up the old coding machine again

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const path = require('path');
 const router = require('express').Router();
 const fs = require('fs');
 const util = require('util');
+
 const uuid = require('./helpers/uuid');
-const db = require('.db/db.json');
+const db = require('./db/db.json');
 
 const PORT = 3001;
 
@@ -17,13 +18,13 @@ app.use(express.static('public'));
 
 // GET Route for notes page
 app.get('/notes', (req, res) => {
-    console.log('notes');
+    console.log("notes");
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-// GET Route for homepage
+// GET Route/catchall for homepage
 app.get('*', (req, res) => {
-    console.log('get *');
+    console.log("get *");
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
@@ -33,13 +34,19 @@ app.listen(PORT, () =>
 
 module.exports = router;
 
-// writeToFile
+// const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`data written to ${destination}`)
+    );
+
 
 // readAndAppend
-fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-    }
+// fs.readFile(file, 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err);
+//     }
 
-}
-)
+// }
+// )
